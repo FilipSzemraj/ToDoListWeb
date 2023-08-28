bc6b9a52ff776a1d1a6191379e7c233b2f78d79b
KarolCioc <karcio453@wp.pl>
2023-08-28 14:59:32 +0200

    nearly to end the css

--- ToDo/script.js
+++ ToDo/script.js
@@ -1,8 +1,14 @@
 // Get references to the popup and buttons
+const openRegisterButton = document.getElementById("registerButton");
+const popupRegister = document.getElementById("registerBox");
 const background_popup = document.querySelector("main");
-const popup = document.getElementById("login_box");
-const openPopupButton = document.getElementById("login_button");
-const closePopupButton = document.getElementById("close_button");
+const popup = document.getElementById("loginBox");
+const openPopupButton = document.getElementById("loginButton");
+const closePopupButton = document.getElementById("closeButton");
+const form_login = document.getElementById("formLogin");
+let isRegisterWindow = false;
+
+const children_popup = popup.children;
 
 // Open the popup when the "Open Popup" button is clicked
 openPopupButton.addEventListener("click", function () {
@@ -19,4 +25,13 @@ window.addEventListener("click", function (event) {
     if (event.target === background_popup) {
         popup.style.display = "none";
     }
+});
+openRegisterButton.addEventListener("click", function () {
+    if(!isRegisterWindow) {
+        popupRegister.style.display = "flex";
+    }
+    else{
+        popupRegister.style.display = "none";
+    }
+    isRegisterWindow=!isRegisterWindow;
 });
